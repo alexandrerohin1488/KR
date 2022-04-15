8319d2c0ad7b5b8dad61a2307120455f7b5117ac
student308 <student308@KHPK.RU>
2022-04-15 13:47:32 +0700

    kontrolnaya

---
+++ KR/KR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace KR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           User user = new User();
+            user.SGEmail = "zhidkost@gmail.com";
+            Console.WriteLine(user.SGEmail);
+            Shop shop = new Shop();
+            User user1 = new User();
+            User user2 = new User();
+            User user3= new User();
+            shop.Add(user1);
+            shop.Remove(user1);
+            shop.Add(user2);
+            shop.Add(user3);
+            shop.Add(user);
+            shop.Notify();
+        }
+    }
+}
